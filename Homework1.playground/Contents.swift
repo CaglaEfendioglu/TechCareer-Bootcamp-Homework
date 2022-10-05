33619bc4fcc83411fa83652aed201efd1864af7f
Cagla Efendioglu <macbookair@Cagla-Efendioglu.local>
2022-10-05 17:25:59 +0300

    Homework

---
+++ Homework1.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var islemNo = 22
+var ogrenciNo = 333
+var kitapNo = 11
+var aTarih = "20.9.202"
+var vTarih = "21.9.2022"
+var ogrenciAd = "Cagla"
+var ogrenciSoyad = "Efendioglu"
+var cinsiyet = "Kadin"
+var dTarih = "22.9.2022"
+var sinif = "12/A"
+let isbnNo = 123456789
+let kitapAdi = "Hayvan Ciftligi"
+let turNo = 33
+let sayfaSayisi = 112
+let puan :Double = 10.0
+let yazarNo :Int = 55
+let yazarAd :String = "George"
+let yazarSoyad :String = "Orwell"
+let turAdi :String = "Hiciv"
+
+print("İslem no         : \(islemNo)")
+print("Ogrenci no       : \(ogrenciNo)")
+print("Kitap no         : \(kitapNo)")
+print("A. Tarih         : \(aTarih)")
+print("V. Tarih         : \(vTarih)")
+print("Ogrenci ad       : \(ogrenciAd)")
+print("Ogrenci Soyad    : \(ogrenciSoyad)")
+print("Cinsiyet         : \(cinsiyet)")
+print("D. Tarih         : \(dTarih)")
+print("Sinif            : \(sinif)")
+print("Isbn No          : \(isbnNo)")
+print("Kitap Adi        : \(kitapAdi)")
+print("Tur No           : \(turNo)")
+print("Sayfa Sayisi     : \(sayfaSayisi)")
+print("Puan             : \(puan)")
+print("Yazar No         : \(yazarNo)")
+print("Yazar Ad         : \(yazarAd)")
+print("Yazar Soyad      : \(yazarSoyad)")
+print("Tur Adi          : \(turAdi)")
